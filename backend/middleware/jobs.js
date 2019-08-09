722063a58353a3ca5710ea8f61ff52c99a63da44
Onoja <onojamatthew59@gmail.com>
2019-08-09 13:34:26 +0000

    updated jobs

--- backend/middleware/jobs.js
+++ backend/middleware/jobs.js
@@ -1,17 +1,19 @@
 const cron = require("node-cron");
 
 
-// Automates weekly fund addition for the group
+// Automatically starts weekly fund accumulation
+// for the group
 exports.weeklyJob = (res, weeklySum, groupId) => {
-  const weekly = new cron.schedule("* * * * *", () => {
+  const weekly = new cron.schedule("* * 7 * *", () => {
     console.log("job started...");
     weeklySum(res, groupId);
   }, null, true, "Abidjan");
 }
 
-// Automates monthly fund remittance to members of the group
+// Automatically remits the weekly accumulated fund of the group
+// to a member on the 30 of every month
 exports.monthlySettlement = (res, monthlySettlement, groupId) => {
-  const monthly = new cron.schedule("2 * * * *", () => {
+  const monthly = new cron.schedule("* * 30 * *", () => {
     console.log("job started in 2 minutes...");
     monthlySettlement(res, groupId);
   }, null, true, "Abidjan");
